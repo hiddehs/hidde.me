a49526eb7cb4366a8f2326c274958498e33ed433
hiddehs <hidde.schultze@gmail.com>
2021-05-08 14:09:14 +0200

    Cleanup and stability improvements

--- components/Header.js
+++ components/Header.js
@@ -9,6 +9,13 @@ export default function Header () {
       if (getLinksOpen) setLinksOpen(0)
     })
   }
+
+  const links = [
+    'work',
+    'experience',
+    'about',
+  ]
+
   return (
     <nav className="fixed w-full top-0 z-50">
       <div className="container my-2">
@@ -19,7 +26,7 @@ export default function Header () {
           <Link href={'/'}><a className={'logo'}><img height="31px"
                                                       src="logo.svg"
                                                       alt="hidde.me Logo"/></a></Link>
-          <button onClick={() => {setLinksOpen(!getLinksOpen)}} aria-label={getLinksOpen ? "Open menu" : "Close menu"}
+          <button onClick={() => {setLinksOpen(!getLinksOpen)}} aria-label={getLinksOpen ? 'Open menu' : 'Close menu'}
                   className={`leading-none hamburger hamburger--minus sm:hidden ${getLinksOpen
                     ? 'is-active'
                     : ''}`}>
@@ -30,11 +37,15 @@ export default function Header () {
           <div className={`links ${getLinksOpen
             ? 'flex'
             : 'hidden'} sm:flex flex-wrap flex-row items-center py-3 md:py-0`}>
-            <AnchorLink offset={50} className="text-3xl block font-bold w-full" href="#work">work</AnchorLink>
-            <AnchorLink offset={50} className="text-3xl block font-bold w-full"
-               href="#experience">experience</AnchorLink>
-            <AnchorLink offset={50} className="text-3xl block font-bold w-full"
-               href="#about">about</AnchorLink>
+
+            {links.map(l => {
+              return (<AnchorLink offset={50} className="text-3xl text-gray-800 sm:my-0 my-2 block font-bold w-full" href={"#" + l}>{l}</AnchorLink>)
+            })}
+
+            {/*<AnchorLink offset={50} className="text-3xl block font-bold w-full"*/}
+            {/*   href="#experience">experience</AnchorLink>*/}
+            {/*<AnchorLink offset={50} className="text-3xl block font-bold w-full"*/}
+            {/*   href="#about">about</AnchorLink>*/}
           </div>
 
         </div>
