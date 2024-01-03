7937df9f8fc80b0cb5a46030215f53c107f2cd48
Hidde Schultze <hidde@hidde.dev>
2024-01-03 09:17:26 +0100

    Remove routing

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -7,7 +7,7 @@ export default function Custom404() {
         <h1 style={{fontSize:"8rem"}} className="font-bold">404</h1>
         <p className={"mb-4"}>Page not found.</p>
         <Link className="btn btn-outline" href={"/"} >
-          go back to home.
+          go back to home.`
         </Link>
       </div>
     </>
